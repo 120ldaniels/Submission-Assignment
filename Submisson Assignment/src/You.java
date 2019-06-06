ec2a04e09163fb35b75caf1c5dcd82cb4ea21471
120ldaniels <120ldaniels@frhsd.com>
2019-06-06 13:39:03 -0400

    Completed Submission-Assignment

--- Submisson Assignment/src/You.java
+++ Submisson Assignment/src/You.java
@@ -1,30 +1,51 @@
 /**
  * 
- * @author //Put your first and last name here
+ * @author Logan Daniels
  * 
  * 
- * Assignment Instructions:
- * 	Write the following constructors and methods:
- * 	- You() : Sets all private global variables to your information
- * 	- String getFullName() : Returns a String of your first and last name with a space in between
- * 	- String setFavoriteAnimal(String animal) : Sets favoriteAnimal to animal and returns it 
- * 	- int happyBirthday() : Prints out "Happy Birthday!", increases age by 1, and returns it
+ *         Assignment Instructions: Write the following constructors and
+ *         methods: - You() : Sets all private global variables to your
+ *         information - String getFullName() : Returns a String of your first
+ *         and last name with a space in between - String
+ *         setFavoriteAnimal(String animal) : Sets favoriteAnimal to animal and
+ *         returns it - int happyBirthday() : Prints out "Happy Birthday!",
+ *         increases age by 1, and returns it
  *
- * Submission Instructions:
- *  - Save your saved work on Eclipse - your workspace should be the file location of the cloned repo
- *  - In GitHub Desktop, add your summary and commit your changes
- *  - Then push your commits to GitHub
- *  - Check your repo on GitHub.com to make sure the files are as you intend
- *  - Add achiu-frhsd as a collaborator
+ *         Submission Instructions: - Save your saved work on Eclipse - your
+ *         workspace should be the file location of the cloned repo - In GitHub
+ *         Desktop, add your summary and commit your changes - Then push your
+ *         commits to GitHub - Check your repo on GitHub.com to make sure the
+ *         files are as you intend - Add achiu-frhsd as a collaborator
  *
  */
-public class You{
+public class You {
 
 	private String firstName;
 	private String lastName;
 	private String favoriteAnimal;
 	private int age;
 
-	// Write your code here
-	
+	You() {
+
+		firstName = "Logan";
+		lastName = "Daniels";
+		favoriteAnimal = "Creeper";
+		age = 16;
+
+	}
+
+	public String getFullName() {
+		return "Logan Daniels";
+	}
+
+	String setFavoriteAnimal(String animal) {
+		favoriteAnimal = animal;
+		return animal;
+	}
+
+	int happyBirthday() {
+		System.out.println("Happy Birthday!");
+		return (age++);
+	}
+
 }
